2dcb421425e58230ad4a5e6aa8f2e9800c285ec5
MatanBenNoach <matanbn22@gmail.com>
2017-08-22 14:14:40 +0300

    Added get_predictions method which gets a dataset and returns a tuple of dataX which will be are samples, and dataY which will be our desired predictions for the samples.

--- data-extractor.py
+++ data-extractor.py
@@ -2,7 +2,7 @@ import numpy as np
 usable_chars = [' ', 'ו', 'י', 'ה', 'מ', 'ל', 'א', 'ר', 'ב', 'נ', 'ת', 'ש', 'ע', 'כ', ',', 'ד', '.', 'ח', 'פ', 'ק',
                 '-', 'צ', 'ג', 'ס', 'ז', '"', 'ט', '?', '!', ':', '\'', '1','2','3','4','5','6','7','8','9','0']
 
-# Converts a letter to a one hot np array according to the usable_chars mapping.
+# Convert a letter to a one hot np array according to the usable_chars mapping.
 def letter_to_vec(letter):
     letter_vec = np.zeros(len(usable_chars))
     letter_vec[usable_chars.index(letter)] = 1
@@ -14,7 +14,16 @@ def letters_to_matrix(letters):
         data.append(letter_to_vec(letter))
     return np.asarray(data)
 
-#Gets a txt file name and returns a tuple of np arrays of one shot np arrays according to the usable_chars mapping.
+# Create the right predictions of each letter in the dataset.
+def get_predictions(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
+
+# Get a txt file name and returns a tuple of np arrays of one shot np arrays according to the usable_chars mapping.
 def read_data(file_name, test_size):
     file = open(file_name, 'r', encoding='utf-8')
     text = file.readline()
