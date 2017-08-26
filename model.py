bf874bac6dc09c733d27beb842276b0ca5e45e1b
MatanBenNoach <matanbn22@gmail.com>
2017-08-26 11:02:41 +0300

    The japanese version of lstm

--- model.py
+++ model.py
@@ -14,7 +14,6 @@ class Model:
 
         self.model.add(TimeDistributed(Dense(y_shape)))
         self.model.add(Activation('softmax'))
-        self.model.compile(loss="categorical_crossentropy", optimizer="rmsprop", metrics=['accuracy'])
         self.history = None
         self.batch_size = batch_size
 
