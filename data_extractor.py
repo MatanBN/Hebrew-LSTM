c6289f92e42522a85bedbc580627ab4ecfe25db8
MatanBenNoach <matanbn22@gmail.com>
2017-08-26 11:58:46 +0300

    Update data_extractor.py

--- data_extractor.py
+++ data_extractor.py
@@ -30,8 +30,9 @@ def format_dataset(data_x, data_y, seq_length=1, max_len=1):
     for i in range(len(data_y)):
         for j in range(len(data_y[i])):
             y[i][j][data_y[i][j]] = 1.0
-
     return x, y
+
+
 
 
 # Create the dataset in the right format for lstm.
@@ -48,22 +49,25 @@ def create_dataset(dataset, look_back=1):
 
 
 """
-Get a txt file name and returns a tuple of np arrays of x_data for train and test and y_data for train and test which
-will be in the format of one shot np arrays according to the usable_chars mapping.
+Get a txt file name and returns a tuple of np arrays of x_data for train, validation and test and y_data for train,
+validation and test which will be in the format of one shot np arrays according to the usable_chars mapping.
 """
 
 
-def read_data(file_name, test_size, sequence=1):
+def read_data(file_name, test_size, validation_size, sequence=1):
     file = open(file_name, 'r', encoding='utf-8')
     text = file.readline()
-    train_size = len(text) - test_size
+    train_size = len(text) - test_size - validation_size
 
     train_text = text[:train_size]
-    test_text = text[train_size:]
+    val_text = text[train_size: train_size + validation_size]
+    test_text = text[train_size + validation_size:]
 
     train_x = letters_to_numbers(train_text)
+    val_x = letters_to_numbers(val_text)
     test_x = letters_to_numbers(test_text)
 
     (train_x, train_y) = create_dataset(train_x, sequence)
-    (test_x, test_y) = create_dataset(test_x, sequence)
-    return (train_x, train_y, test_x, test_y)
+    (val_x, val_y) = create_dataset(val_x, sequence)
+    (test_x, test_y) = create_dataset(test_x, look_back=1)
+    return (train_x, train_y, val_x, val_y, test_x, test_y)
